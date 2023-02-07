7a1ae6d0430015552a276e99fbdcc4701e1b1db4
chmilkka <mishachmil228335@gmail.com>
2023-02-07 14:39:31 +0200

    add HT3

---
+++ HomeTask3/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+
+namespace HomeTask3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AuthenticationIfElse();
+            AuthenticationSwitch();
+            AuthenticationSwitch();
+        }
+        public static void AuthenticationIfElse()
+        {
+            const string secretPassword = "12345";
+
+            Console.WriteLine("Please, enter your login:");
+            string loginInput = Console.ReadLine();
+
+            if (loginInput == "USER")
+            {
+                Console.WriteLine("Please, enter your password:");
+                string passwordInput = Console.ReadLine();
+                if (passwordInput == secretPassword)
+                {
+                    Console.WriteLine("Hello User!");
+                }
+                else
+                {
+                    Console.WriteLine("Password is not correct");
+                }
+            }
+            else if (loginInput == "ADMIN")
+            {
+                Console.WriteLine("Please, enter your password:");
+                string passwordInput = Console.ReadLine();
+                if (passwordInput == secretPassword)
+                {
+                    Console.WriteLine("Hello Administrator!");
+                }
+                else
+                {
+                    Console.WriteLine("Password is not correct");
+                }
+            }
+            else if (loginInput == "")
+            {
+                Console.WriteLine("Hello Anonimous!");
+            }
+        }
+        public static void AuthenticationTernary()
+        {
+            const string secretPassword = "12345";
+
+            Console.WriteLine("Please, enter your login:");
+            string loginInput = Console.ReadLine();
+
+            if (loginInput == "USER")
+            {
+                Console.WriteLine("Please, enter your password:");
+                string passwordInput = Console.ReadLine();
+                string message = passwordInput == secretPassword ? "Hello User!" : "Password is not correct";
+                Console.WriteLine(message);
+            }
+            else if (loginInput == "ADMIN")
+            {
+                Console.WriteLine("Please, enter your password:");
+                string passwordInput = Console.ReadLine();
+                string message = passwordInput == secretPassword ? "Hello Administrator!" : "Password is not correct";
+                Console.WriteLine(message);
+            }
+            else if (loginInput == "")
+            {
+                Console.WriteLine("Hello Anonimous!");
+            }
+
+
+        }
+        public static void AuthenticationSwitch()
+        {
+            const string secretPassword = "12345";
+
+            Console.WriteLine("Please, enter your login:");
+            string loginInput = Console.ReadLine();
+
+            switch(loginInput)
+            {
+                case "USER":
+                    Console.WriteLine("Please, enter your password:");
+                    string passwordInput = Console.ReadLine();
+                    switch(passwordInput)
+                    {
+                        case secretPassword:
+                            Console.WriteLine("Hello User!");
+                            break;
+                        default:
+                            Console.WriteLine("Password is not correct");
+                            break;
+                    }
+                    break;
+
+                case "ADMIN":
+                    Console.WriteLine("Please, enter your password:");
+                    passwordInput = Console.ReadLine();
+                    switch (passwordInput)
+                    {
+                        case secretPassword:
+                            Console.WriteLine("Hello Administrator!");
+                            break;
+                        default:
+                            Console.WriteLine("Password is not correct");
+                            break;
+                    }
+                    break;
+
+                case "":
+                    Console.WriteLine("Hello Anonimous!");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
